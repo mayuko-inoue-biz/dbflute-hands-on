e6640f936925d9d2028092882133b9854d92cb4f
jflute <dbflute@gmail.com>
2025-01-14 19:12:08 +0900

    step2のレビュー

--- src/test/java/org/docksidestage/handson/exercise/HandsOn02Test.java
+++ src/test/java/org/docksidestage/handson/exercise/HandsOn02Test.java
@@ -37,6 +37,7 @@ public class HandsOn02Test extends UnitContainerTestCase {
      */
     public void test_searchByMemberNamePrefixS() throws Exception {
         // ## Arrange ##
+        // TODO mayukorin [いいね] 良い変数名、わかりやすい！ by jflute (2025/01/14)
         String memberNamePrefix = "S";
 
         // ## Act ##
@@ -46,8 +47,13 @@ public class HandsOn02Test extends UnitContainerTestCase {
         });
 
         // ## Assert ##
+        // TODO mayukorin [いいね] ちゃんと素通り防止できてますね！ by jflute (2025/01/14)
+        // ちなみに、UnitTestでループするときはずっとそうなので、assH -> assertHasAnyElement()
+        // という専用のメソッドあるので、そっちを使ってみてください。
         assertFalse(memberList.isEmpty());
         memberList.forEach(member -> {
+            // TODO mayukorin 細かいですが、member.getMemberName() が二度呼ばれてコードが横長なので... by jflute (2025/01/14)
+            // 変数に抽出してみましょう。IntelliJのショートカットがあるはずです。
             log("memberName: {}, memberNamePrefixForSearch: {}", member.getMemberName(), memberNamePrefix);
             assertTrue(member.getMemberName().startsWith(memberNamePrefix));
         });
@@ -58,6 +64,7 @@ public class HandsOn02Test extends UnitContainerTestCase {
      */
     public void test_searchByMemberIdOne() throws Exception {
         // ## Arrange ##
+        // TODO mayukorin 99999は一時的なお試しで、コミットはしないようにしましょう by jflute (2025/01/14)
         Integer memberId = 99999;
 
         // ## Act ##
@@ -66,8 +73,12 @@ public class HandsOn02Test extends UnitContainerTestCase {
         });
 
         // ## Assert ##
+        // TODO mayukorin 丁寧で明示的で悪くはないですが、この場合 DBFlute の get() で例外の方がデバッグしやすいですね by jflute (2025/01/14)
+        // assertTrue()を削除して99999で実行して例外メッセージを読んでみましょう。
         assertTrue(member.isPresent()); // memberがnullのときに下のgetMemberId()でExceptionにならないようにisPresent()を追加したが、不要かも
+        // TODO mayukorin せめて member.get() は変数に抽出しちゃった方がスッキリするかと思います by jflute (2025/01/14)
         assertEquals(memberId, member.get().getMemberId());
+        // TODO mayukorin テスト内でログを出すとき、assertの前に出す方が落ちたときにそのログを見てデバッグできます by jflute (2025/01/14)
         log("memberName: {}, memberId: {}, memberIdForSearch", member.get().getMemberName(), member.get().getMemberId(), memberId);
     }
 
@@ -84,6 +95,7 @@ public class HandsOn02Test extends UnitContainerTestCase {
         });
 
         // ## Assert ##
+        // TODO mayukorin こっちも素通り防止を by jflute (2025/01/14)
         memberList.forEach(member -> {
             log("memberId: {}, birthDate: {}", member.getMemberName(), member.getBirthdate());
             assertNull(member.getBirthdate());
