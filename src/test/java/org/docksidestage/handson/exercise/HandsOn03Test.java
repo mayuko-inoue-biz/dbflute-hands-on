e7e365425bf2c8ed4ce58daa48b70c76c1475367
inouemayuko <m.inoue@bizreach.co.jp>
2025-01-19 21:50:12 +0900

    ハンズオン03のSilverを一旦解いてみた

---
+++ src/test/java/org/docksidestage/handson/exercise/HandsOn03Test.java
@@ -0,0 +1,131 @@
+package org.docksidestage.handson.exercise;
+
+import java.time.LocalDate;
+
+import javax.annotation.Resource;
+
+import org.dbflute.cbean.result.ListResultBean;
+import org.dbflute.optional.OptionalEntity;
+import org.docksidestage.handson.dbflute.exbhv.MemberBhv;
+import org.docksidestage.handson.dbflute.exentity.Member;
+import org.docksidestage.handson.dbflute.exentity.MemberSecurity;
+import org.docksidestage.handson.dbflute.exentity.MemberStatus;
+import org.docksidestage.handson.unit.UnitContainerTestCase;
+
+/**
+ * DBFluteハンズオン03のためのクラス
+ * @author mayukorin
+ */
+public class HandsOn03Test extends UnitContainerTestCase {
+
+    @Resource
+    private MemberBhv memberBhv;
+
+    /**
+     * 会員名称がSで始まる1968年1月1日以前に生まれた会員を検索する <br>
+     * o 上記条件に合致する会員がDBに存在するか <br>
+     * o 検索した会員が上記条件に合致するか <br>
+     * をチェックしたい <br>
+     * また、1968年1月1日は含まれることに注意。
+     */
+    public void test_searchMemberByNamePrefixAndBirthDate() throws Exception {
+        // ## Arrange ##
+        String memberNamePrefixForSearch = "S";
+        LocalDate birthdateForSearch = LocalDate.of(1968, 1, 1);
+    
+        // ## Act ##
+        ListResultBean<Member> memberList = memberBhv.selectList(cb -> {
+            cb.setupSelect_MemberStatus();
+            cb.specify().columnMemberName();
+            cb.specify().columnBirthdate();
+            cb.specify().specifyMemberStatus();
+            cb.query().setMemberName_LikeSearch(memberNamePrefixForSearch, op -> op.likePrefix());
+            cb.query().setBirthdate_FromTo(null, birthdateForSearch, op -> {
+                op.allowOneSide(); // 指定日以前に生まれた人を検索したくて、何日以降に生まれたかは問わない
+                op.compareAsDate();
+            });
+            cb.query().addOrderBy_Birthdate_Asc();
+        });
+
+        // ## Assert ##
+        assertHasAnyElement(memberList);
+        memberList.forEach(member -> {
+            String memberName = member.getMemberName();
+            LocalDate memberBirthdate = member.getBirthdate();
+            MemberStatus memberStatus = member.getMemberStatus().get();
+            log("memberName: {}, memberBirthDate: {}, memberStatusCodeName: {}, memberNamePrefixForSearch: {}, fromBirthDateForSearch: {}", memberName, memberBirthdate, memberStatus.getMemberStatusName(), memberNamePrefixForSearch, birthdateForSearch);
+            assertTrue(memberName.startsWith(memberNamePrefixForSearch));
+            assertTrue(memberBirthdate.isBefore(birthdateForSearch.plusDays(1))); // 生年月日が指定日時ぴったりでもOK
+        });
+    }
+
+    /**
+     * 会員ステータスと会員セキュリティ情報も取得して会員を検索 <br>
+     * o 会員が存在すること <br>
+     * o 会員ステータスと会員セキュリティ情報が存在すること <br>
+     * をチェックしたい <br>
+     * また、カージナリティを意識したい
+     */
+    public void test_searchMemberWithStatusAndSecurityInfo() throws Exception {
+        // ## Arrange ##
+    
+        // ## Act ##
+        ListResultBean<Member> memberList = memberBhv.selectList(cb -> {
+            cb.setupSelect_MemberStatus();
+            cb.setupSelect_MemberSecurityAsOne();
+            cb.specify().columnMemberId();
+            cb.specify().columnBirthdate();
+            cb.specify().specifyMemberStatus();
+            cb.specify().specifyMemberServiceAsOne();
+            cb.query().addOrderBy_Birthdate_Desc();
+            cb.query().addOrderBy_MemberId_Asc();
+        });
+    
+        // ## Assert ##
+        assertHasAnyElement(memberList);
+        memberList.forEach(member -> {
+            Integer memberId = member.getMemberId();
+            LocalDate memberBirthdate = member.getBirthdate();
+            OptionalEntity<MemberStatus> optMemberStatus = member.getMemberStatus();
+            OptionalEntity<MemberSecurity> optMemberSecurity = member.getMemberSecurityAsOne();
+            log("memberId: {}, birthDate: {}, memberStatus: {}, memberSecurity: {}", memberId, memberBirthdate, optMemberStatus, optMemberSecurity);
+            assertTrue(optMemberStatus.isPresent());
+            assertTrue(optMemberSecurity.isPresent());
+        });
+    }
+
+    /**
+     * 会員セキュリティ情報のリマインダ質問で2という文字が含まれている会員を検索 <br>
+     * o 上に該当する会員が存在すること <br>
+     * o リマインダ質問に2が含まれていること <br>
+     * をチェックしたい <br>
+     * ただし、検索時に会員セキュリティ情報のデータ自体は要らない
+     */
+    public void test_searchMemberBySecurityInfoReminderQuestion() throws Exception {
+        // ## Arrange ##
+        String reminderQuestionKeyword = "2";
+    
+        // ## Act ##
+        ListResultBean<Member> memberList = memberBhv.selectList(cb -> {
+            cb.specify().columnMemberId();
+            cb.specify().columnMemberName();
+            cb.query().queryMemberSecurityAsOne().setReminderQuestion_LikeSearch(reminderQuestionKeyword, op -> op.likeContain()); // dbfluteでは、関連テーブルを使いたい目的に対応するメソッドがあるらしい。今回の目的はカラム取得ではなく絞り込みなので、setUpSelect使う必要なし。
+        });
+    
+        // ## Assert ##
+        assertHasAnyElement(memberList);
+        memberList.forEach(member -> {
+            Integer memberId = member.getMemberId();
+
+            // Assertするために、MemberSecurityInfoを取ってくる（でもmember分検索してしまっているの微妙かも）
+            Member memberSelectedById = memberBhv.selectEntity(cb -> {
+                cb.setupSelect_MemberSecurityAsOne();
+                cb.query().setMemberId_Equal(memberId);
+            }).get();
+            MemberSecurity memberSecurity = memberSelectedById.getMemberSecurityAsOne().get(); // memberSecurityは存在すること前提（1個前のテストでそれをassertしてる）
+
+            log("memberId: {}, memberSecurity: {}", memberId, memberSecurity);
+            assertContains(memberSecurity.getReminderQuestion(), reminderQuestionKeyword);
+        });
+    }
+}
