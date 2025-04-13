c3dd83ca68a2a5e07d3b47a2e44b6b562cfd7981
inouemayuko <m.inoue@bizreach.co.jp>
2025-04-13 21:34:48 +0900

    有効期間が一日だけ重複しているパターンも検出するようにした

--- dbflute_maihamadb/playsql/take-finally.sql
+++ dbflute_maihamadb/playsql/take-finally.sql
@@ -1,6 +1,7 @@
 -- #df:assertListZero#
 -- /- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 -- Member addresses should be only one at any time.
+-- 有効期間が一日だけ重複しているパターンも検出（begin_dateとend_dateが同一のパターンを検出すれば良い）
 -- - - - - - - - - - -/
 select adr.MEMBER_ADDRESS_ID, adr.MEMBER_ID
      , adr.VALID_BEGIN_DATE, adr.VALID_END_DATE
@@ -10,7 +11,7 @@ where exists (select subadr.MEMBER_ADDRESS_ID
               from MEMBER_ADDRESS subadr
               where subadr.MEMBER_ID = adr.MEMBER_ID
                 and subadr.VALID_BEGIN_DATE > adr.VALID_BEGIN_DATE
-                and subadr.VALID_BEGIN_DATE < adr.VALID_END_DATE
+                and subadr.VALID_BEGIN_DATE <= adr.VALID_END_DATE
 )
 ;
 -- #df:assertListZero#
